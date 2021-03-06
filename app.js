e17443513dba1f07b53b303924f55f22a5eaaad4
mmanso <mayara.manso@mercadolivre.com>
2021-03-06 14:03:34 -0300

    fix lint

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-
 const express = require('express');
 const app = express();
 
 
 app.use(express.static('public'));
 
-app.get('/', (req, res )=>{
+app.get('/', (req, res) => {
 
     console.log(req.url)
     res.sendFile(__dirname + '/views/home.html');
-} );
+});
 
-app.listen(3000, ()=> console.log('server lingado'));
+app.listen(3000, () => console.log('server lingado'));
