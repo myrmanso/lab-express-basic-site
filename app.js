aa8d2ad07ed751d9460745d1ffc878e7572d082b
Antonio Vitale <vitalb2b@Antonios-MacBook-Air.local>
2021-03-06 13:59:17 -0300

    lab pending

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res )=>{
+
+    console.log(req.url)
+    res.sendFile(__dirname + '/views/home.html');
+} );
+
+app.listen(3000, ()=> console.log('server lingado'));
