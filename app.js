917637a44b23cbf93ed57e9e24b4a8ce6a97cde2
mmanso <mayara.manso@mercadolivre.com>
2021-03-06 14:56:55 -0300

    creates route about

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.get('/', (req, res) => {
     console.log(req.url)
     res.sendFile(__dirname + '/views/home.html');
 });
+app.get('/about', (req, res) => {
+
+    console.log(req.url)
+    res.sendFile(__dirname + '/views/about.html');
+});
 
 app.listen(3000, () => console.log('server lingado'));
